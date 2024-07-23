3eb89e9dc73ec50e23f11f3a3f808c8c8190e75f
metamonk <metamonk@ratlabs.xyz>
2024-07-23 02:07:42 -0500

    feat: add cheers chad

--- app/chad/page.tsx
+++ app/chad/page.tsx
@@ -4,8 +4,8 @@ export default function ChadEyes() {
   return (
     <main className="max-w-[500px] mx-auto md:p-0 px-4 mt-4">
       <div className="mb-4">
-        <h1 className="text-2xl font-bold">Cheers Pilled 🍸</h1>
-        <p className="text-sm text-gray-500">Add a cheers to your image</p>
+        <h1 className="text-2xl font-bold">Cheers Chad 💥</h1>
+        <p className="text-sm text-gray-500">Become a Cheers Chad</p>
       </div>
       <AddChad />
     </main>
